ac56d6973770dbd76a2cdba2eb439a586fd844f0
Mauricio Knabben <91981855+MauKnabbenOfficial@users.noreply.github.com>
2024-12-11 16:37:35 -0300

    Home Alterada

--- TrabalhoFinalFrontend/src/App.vue
+++ TrabalhoFinalFrontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <header>
       <nav>
-        <router-link to="/">Home</router-link>
+        <router-link to="/">HomeAlterada</router-link>
         <!-- <router-link to="/about">About</router-link> -->
       </nav>
     </header>
