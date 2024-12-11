aab28ad140fabd188b896cfa504b9748e8a1c543
Mauricio Knabben <91981855+MauKnabbenOfficial@users.noreply.github.com>
2024-12-11 20:14:52 -0300

    teste

--- TrabalhoFinalFrontend/src/App.vue
+++ TrabalhoFinalFrontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <header>
       <nav>
-        <router-link to="/">Home</router-link>
+        <router-link to="/">Home Teste Dev</router-link>
         <!-- <router-link to="/about">About</router-link> -->
       </nav>
     </header>
